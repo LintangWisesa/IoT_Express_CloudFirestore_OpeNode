b59e68f26390ce0ec6d455313f6161ca79c54179
lintangwisesa <lintangwisesa@ymail.com>
2018-09-23 20:27:49 +0700

    express firestore

--- routes/fire.js
+++ routes/fire.js
@@ -1,12 +1,18 @@
-import firebase from 'firebase'
+var firebase = require('firebase')
 // change lines below with your own Firebase snippets!
 var config = {
-  apiKey: "1a2b3c4d5e6f7g8h9i0j",
-  authDomain: "lintang.firebaseapp.com",
-  databaseURL: "https://lintang.firebaseio.com",
-  projectId: "lintang",
-  storageBucket: "lintang.appspot.com",
-  messagingSenderId: "1234567890"
+    apiKey: "AIzaSyDSLRypcYx2yHSqpVXJMBrzYw1xAHZtgYI",
+    authDomain: "test-iot-esp.firebaseapp.com",
+    databaseURL: "https://test-iot-esp.firebaseio.com",
+    projectId: "test-iot-esp",
+    storageBucket: "test-iot-esp.appspot.com",
+    messagingSenderId: "467772798973"
+//   apiKey: "1a2b3c4d5e6f7g8h9i0j",
+//   authDomain: "lintang.firebaseapp.com",
+//   databaseURL: "https://lintang.firebaseio.com",
+//   projectId: "lintang",
+//   storageBucket: "lintang.appspot.com",
+//   messagingSenderId: "1234567890"
 };
 const fire = firebase.initializeApp(config);
-export default fire;
+module.exports = fire;
--- routes/renderer.js
+++ routes/renderer.js
@@ -6,4 +6,27 @@ router.get('/', function (req, res) {
 	return res.send("<h1>Tes Express & Firebase Cloud Firestore</h1>");
 });
 
+router.get('/data', (req, res)=>{
+	const db = fire.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    var wholeData = []
+	// db.collection('ninjas').orderBy('name', 'asc').get()
+	db.collection('lin_esp8266').get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        // console.log(doc.id, '=>', doc.data());
+        // console.log(doc.data().name + doc.data().age);
+        // console.log(doc.data());
+        wholeData.push(doc.data())
+      });
+      console.log(wholeData)
+      res.send(wholeData)
+    })
+    .catch(error => {
+      console.log('Error!', error);
+    })
+})
+
 module.exports = router;
