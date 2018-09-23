9082e66676bf4be6dcf84b94a57e86cdb62f89dd
lintangwisesa <lintangwisesa@ymail.com>
2018-09-23 19:10:07 +0700

    express firestore

--- routes/renderer.js
+++ routes/renderer.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fire = require('./fire')
 
 router.get('/', function (req, res) {
 	return res.send("<h1>Tes Express & Firebase Cloud Firestore</h1>");
